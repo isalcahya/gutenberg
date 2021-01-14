786c676446acf9d953e7ebb8cd287bad80cdf843
isalcahya <cahyaisal13@gmail.com>
2021-01-14 05:48:44 +0000

    add redis object cache

---
+++ rcwp-redis-config.php
@@ -0,0 +1,20 @@
+<?php 
+return array (
+  'type' => 'single_domain',
+  'domain' => 'wp-isal.dgqhsazcdz-rz83y2jxz6d7.p.runcloud.link',
+  'path' => '',
+  'status' => 1,
+  'prefix' => 'd03d0ad5f8054c',
+  'maxttl' => 86400,
+  'ignored_groups' => 
+  array (
+    0 => 'counts',
+    1 => 'plugins',
+    2 => 'themes',
+    3 => 'comment',
+    4 => 'wc_session_id',
+    5 => 'bp_notifications',
+    6 => 'bp_messages',
+    7 => 'bp_pages',
+  ),
+);
